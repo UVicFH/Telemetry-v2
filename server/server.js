3ddbbcb5b0e03fb0c0f65e0ea1afab34e7b8ed47
Brendon Earl <bearl@uvic.ca>
2016-06-12 13:34:49 -0700

    Add express server w/ hello world

--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
-/**
- * Created by Brendon on 2016-06-11.
- */
+// var http = require('http');
+var express = require('express');
+var app = express();
+
+var PATH = '/../public/';
+var PORT = 3000;
+
+app.get('/', function (res, req, next) {
+
+    var options = {
+        root: __dirname + PATH,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    req.sendFile('index.html', options, function(err) {
+        if (err) console.log('GET ERROR');
+        else console.log('GET index');
+    });
+});
+
+app.listen(PORT, function reportRunning() {
+    console.log('Running on port ' + PORT);
+});
