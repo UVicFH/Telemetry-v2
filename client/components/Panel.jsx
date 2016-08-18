746de70fdfdbb55b7b483d45f837054a957422ea
Brendon Earl <bearl@uvic.ca>
2016-08-18 01:09:45 -0700

    Fix refactoring issues (Frame => Panel)

--- client/components/Panel.jsx
+++ client/components/Panel.jsx
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react'
 
-export default class Frame extends Component {
+export default class Panel extends Component {
 	constructor (props) {
 		super (props)
 	}
